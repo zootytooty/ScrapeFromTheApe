c2bb95dcf4edd2e2f81b2ee53c696598fccaba54
Trent Howard <trent.howard.42@gmail.com>
2019-03-10 17:07:17 +1100

    Extract image url scrape first 4 pages of timeline

--- scrape_from_the_ape/spiders/jazzlab.py
+++ scrape_from_the_ape/spiders/jazzlab.py
@@ -4,7 +4,13 @@ import scrapy
 
 class JazzlabSpider(scrapy.Spider):
     name = 'jazzlab'
-    start_urls = ['https://jazzlab.club//']
+    base_url = 'https://jazzlab.club'
+    page = 0
+    start_urls = []
+
+    while page < 48:
+        start_urls.append(f'{base_url}/?layout=timeline&start={page}')
+        page +=12
 
     custom_settings={ 'FEED_URI': "jazzlab.json",
                        'FEED_FORMAT': 'json'}
@@ -31,7 +37,8 @@ class JazzlabSpider(scrapy.Spider):
             gig['title'] = div.css(".eb-event-title span[itemprop*='name']::text").extract_first()
             gig['price'] = div.css(".eb-individual-price::text").extract_first()
             gig['blurb'] = ' '.join([x for x in div.css(".eb-description-details p::text").extract()])
-            gig['url'] = "{}{}".format(self.start_urls[0], div.css(".eb-event-title::attr(href)").extract_first())
+            gig['url'] = "{}{}".format(self.base_url, div.css(".eb-event-title::attr(href)").extract_first())
+            gig['imageUrl'] = "{}{}".format(self.base_url, div.css(".eb-modal::attr(href)").extract_first())
 
 
             yield gig
