6330dfee5872382418d2a15a9252318490a136fb
yassin-eltahir <yeltahir@gmail.com>
2019-04-22 10:38:21 +1000

    Update to capture all items & send to DB via API

--- scrape_from_the_ape/pipelines.py
+++ scrape_from_the_ape/pipelines.py
@@ -1,69 +1,22 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-import yaml
-import psycopg2
-import MySQLdb as mysql
 import dateparser
+import requests
 
 class ScrapeFromTheApePipeline(object):
 
-    def open_spider(self, spider):
-
-        conf = yaml.load(open('conf.yaml', 'r'))
-
-        hostname = conf['hostname']
-        username = conf['username']
-        password = conf['password']
-        database = conf['database']
-        dbtype = conf['dbtype']
-
-        if dbtype == 'postgres':
-          self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-        elif dbtype == 'mysql':
-          self.connection = mysql.connect(host=hostname, user=username, passwd=password, db=database, charset='utf8')
-        else:
-          raise Exception('[ScrapeFromTheApePipeline] Database type not recognised or empty, please provide dbtype key:value in conf.yaml')
-
-        self.cur = self.connection.cursor()
+    def __init__(self):
+        self.all_gigs = []
 
 
     def close_spider(self, spider):
-        self.cur.close()
-        self.connection.close()
 
+        response = requests.post(url = "https://4xo55t0ma9.execute-api.ap-southeast-2.amazonaws.com/dev/gigmanagement/addgigs", 
+                                json = self.all_gigs)
 
+    
     def process_item(self, item, spider):
-        parsed_date = dateparser.parse(item['date'])
-        str_date = '0000-00-00'
-        if parsed_date is not None:
-            str_date = "{}-{}-{}".format(parsed_date.year,parsed_date.month,parsed_date.day)
-			
-        insert_qry = """
-            insert into gig_guide_tmp (venue, title, music_starts, doors_open, performance_date, price, description, url, image_url) 
-            values('{}','{}','{}','{}', '{}' ,'{}','{}','{}','{}')
-            """.format(
-                item['venue'],
-                item['title'].replace("'",""),
-                item['music_starts'],
-                item['doors_open'].replace("'",""),
-                str_date,
-                item['price'],
-                item['desc'].replace("'",""),
-                item['url'],
-                item['image_url']
-            )
 
-        try:
-            self.cur.execute(insert_qry)
-            self.connection.commit()
-        except:
-            print(" ")
-            print(insert_qry)
-
-        
+        # Capture item to store once all scraping is complete
+        # noting they must be dictionaries in order to send to the API
+        self.all_gigs.append(dict(item))
+    
         return item
